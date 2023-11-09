0e1c64f516563da8c7ae75c9055490c8697a20fb
dukcode <duk9741@gmail.com>
2023-11-09 22:31:11 +0900

    test: 방문 날짜 요청 DTO 테스트 추가

---
+++ src/test/java/christmas/controller/dto/ReservationDateCreateRequestTest.java
@@ -0,0 +1,38 @@
+package christmas.controller.dto;
+
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ReservationDateCreateRequestTest {
+
+    @Test
+    public void 방문_날짜로_ReservationDateCreateRequest_클래스를_생성할_수_있다() throws Exception {
+        // given
+        String dayOfMonth = "1";
+
+        // when
+        // then
+        assertThatCode(() -> {
+            new ReservationDateCreateRequest(dayOfMonth);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 숫자_형식이_아닌_방문_날짜로_ReservationDateCreateRequest_클래스를_생성시_예외가_발생한다() throws Exception {
+        // given
+        String dayOfMonth = "십삼일";
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new ReservationDateCreateRequest(dayOfMonth);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
