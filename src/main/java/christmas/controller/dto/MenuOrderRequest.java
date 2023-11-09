ad73d1684462c3808f53073a2d21a8edd99f1398
dukcode <duk9741@gmail.com>
2023-11-09 23:26:21 +0900

    refactor: MenuOrderRequest 멤버 변수 List가지도록 수정

--- src/main/java/christmas/controller/dto/MenuOrderRequest.java
+++ src/main/java/christmas/controller/dto/MenuOrderRequest.java
@@ -2,39 +2,56 @@ package christmas.controller.dto;
 
 import christmas.controller.dto.validator.NumberFormatValidator;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class MenuOrderRequest {
 
     private static final String MENU_SEPARATOR = ",";
     private static final String MENU_QUANTITY_DELIMITER = "-";
-    private Map<String, Integer> orders = new HashMap<>();
+
+    private final List<String> menuNames;
+    private final List<Integer> orderCounts;
 
     public MenuOrderRequest(String orders) {
-        validateOrder(orders);
-        Arrays.stream(orders.split(MENU_SEPARATOR))
-                .forEach(order -> {
+        this.menuNames = parseMenuNames(orders);
+
+        List<String> orderCounts = parseOrderCounts(orders);
+        validateOrderCounts(orderCounts);
+
+        this.orderCounts = orderCounts.stream()
+                .map(Integer::valueOf)
+                .toList();
+    }
+
+    private List<String> parseMenuNames(String orders) {
+        return Arrays.stream(orders.split(MENU_SEPARATOR))
+                .map(order -> {
                     String[] tokens = order.trim()
                             .split(MENU_QUANTITY_DELIMITER);
-                    String menu = tokens[0].trim();
-                    int orderQuantity = Integer.parseInt(tokens[1].trim());
-                    this.orders.put(menu, orderQuantity);
-                });
+                    return tokens[0].trim();
+                }).toList();
     }
 
-
-    private void validateOrder(String orders) {
-        Arrays.stream(orders.split(MENU_SEPARATOR))
-                .forEach(order -> {
-                    String orderQuantity = order.trim()
-                            .split(MENU_QUANTITY_DELIMITER)[1]
-                            .trim();
-                    NumberFormatValidator.validate(orderQuantity);
-                });
+    private List<String> parseOrderCounts(String orders) {
+        return Arrays.stream(orders.split(MENU_SEPARATOR))
+                .map(order -> {
+                    String[] tokens = order.trim()
+                            .split(MENU_QUANTITY_DELIMITER);
+                    return tokens[1].trim();
+                }).toList();
     }
 
-    public Map<String, Integer> getOrders() {
-        return orders;
+    private void validateOrderCounts(List<String> orderCounts) {
+        for (String orderCount : orderCounts) {
+            NumberFormatValidator.validate(orderCount);
+        }
+    }
+
+    public List<String> getMenuNames() {
+        return menuNames;
+    }
+
+    public List<Integer> getOrderCounts() {
+        return orderCounts;
     }
 }
