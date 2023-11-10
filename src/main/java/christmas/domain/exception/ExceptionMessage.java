1542b72b08b5958bbe1ddc0f48a159a4906dd2ed
dukcode <duk9741@gmail.com>
2023-11-11 00:52:03 +0900

    test: 음료만으로 주문 시 예외 발생 테스트 추가

--- src/main/java/christmas/domain/exception/ExceptionMessage.java
+++ src/main/java/christmas/domain/exception/ExceptionMessage.java
@@ -3,6 +3,7 @@ package christmas.domain.exception;
 public class ExceptionMessage {
     public static final String INVALID_DATE_RANGE = "유효하지 않은 날짜입니다.";
     public static final String INVALID_ORDER = "유효하지 않은 주문입니다.";
+    public static final String MAX_ORDER_QUANTITY_EXCEEDED = "최대 주문 가능 갯수를 초과했습니다.";
 
     private ExceptionMessage() {
     }
--- src/test/java/christmas/domain/MenuOrdersTest.java
+++ src/test/java/christmas/domain/MenuOrdersTest.java
@@ -53,6 +53,20 @@ class MenuOrdersTest {
     }
 
     @Test
+    public void 음료만으로_구성된_메뉴로_MenuOrders_를_생성하면_예외가_발생한다() throws Exception {
+        // given
+        List<MenuQuantity> menuQuantities = List.of(new MenuQuantity(Menu.ZERO_COKE.getName(), 1),
+                new MenuQuantity(Menu.CHAMPAGNE, 2),
+                new MenuQuantity(Menu.RED_WINE, 3));
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new MenuOrders(menuQuantities);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     public void 주문_총_금액을_계산할_수_있다() throws Exception {
         // given
 
