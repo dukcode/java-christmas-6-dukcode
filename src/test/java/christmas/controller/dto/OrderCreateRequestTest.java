925dd612c5d0cc9392826a65cc5606d52a6868c2
dukcode <duk9741@gmail.com>
2023-11-14 01:33:56 +0900

    test: OrderCreateRequestTest 테스트 메서드명 변경

--- src/test/java/christmas/controller/dto/OrderCreateRequestTest.java
+++ src/test/java/christmas/controller/dto/OrderCreateRequestTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class OrderCreateRequestTest {
 
     @Test
-    public void 메뉴_목록으로_MenuOrderRequests_클래스를_생성을_할_수_있다() throws Exception {
+    public void 메뉴_목록으로_OrderCreateRequest_클래스를_생성을_할_수_있다() throws Exception {
         // given
         String orders = "시저샐러드-1,티본스테이크-1";
 
@@ -25,7 +25,7 @@ class OrderCreateRequestTest {
     }
 
     @Test
-    public void 메뉴_목록에_공백이_있어도_MenuOrderRequests_클래스를_생성을_할_수_있다() throws Exception {
+    public void 메뉴_목록에_공백이_있어도_OrderCreateRequest_클래스를_생성을_할_수_있다() throws Exception {
         // given
         String orders = "시저샐러드-1,     티본스테이크-1";
 
@@ -38,7 +38,7 @@ class OrderCreateRequestTest {
     }
 
     @Test
-    public void 메뉴_주문_갯수가_숫자_형식이_아니면_MenuOrderRequests_생성_시_예외가_발생한다() throws Exception {
+    public void 메뉴_주문_갯수가_숫자_형식이_아니면_OrderCreateRequest_생성_시_예외가_발생한다() throws Exception {
         // given
         String orders = "시저샐러드-a, 티본스테이크-1";
 
@@ -51,7 +51,7 @@ class OrderCreateRequestTest {
     }
 
     @Test
-    public void 메뉴_주문_구분자가_올바른_형식이_아니면_MenuOrderRequests_생성_시_예외가_발생한다() throws Exception {
+    public void 메뉴_주문_구분자가_올바른_형식이_아니면_OrderCreateRequest_생성_시_예외가_발생한다() throws Exception {
         // given
         String orders = "시저샐러드-a & 티본스테이크-1";
 
