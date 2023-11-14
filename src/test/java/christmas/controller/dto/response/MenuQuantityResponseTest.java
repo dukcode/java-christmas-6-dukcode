a45eb9c5f9a4273ba6e62c9c5e3d4b8b5d445047
dukcode <duk9741@gmail.com>
2023-11-14 23:20:07 +0900

    test: MenuQuantityResponse 테스트 추가

---
+++ src/test/java/christmas/controller/dto/response/MenuQuantityResponseTest.java
@@ -0,0 +1,43 @@
+package christmas.controller.dto.response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import christmas.domain.Menu;
+import christmas.domain.MenuQuantity;
+import christmas.domain.MenuType;
+import christmas.domain.Money;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MenuQuantityResponseTest {
+
+    @Test
+    public void MenuQuantity_로_생성할_수_있다() throws Exception {
+        // given
+        Menu menu = new Menu("메뉴", Money.of(10_000L), MenuType.MAIN_DISH);
+        MenuQuantity menuQuantity = new MenuQuantity(menu, 3);
+
+        // when
+        // then
+        assertThatCode(() -> {
+            MenuQuantityResponse.from(menuQuantity);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void toString_메서드_호출_시_정해진_형식을_반환한다() throws Exception {
+        Menu menu = new Menu("메뉴", Money.of(10_000L), MenuType.MAIN_DISH);
+        MenuQuantity menuQuantity = new MenuQuantity(menu, 3);
+
+        // when
+        MenuQuantityResponse menuQuantityResponse = MenuQuantityResponse.from(menuQuantity);
+        String string = menuQuantityResponse.toString();
+
+        // then
+        assertThat(string).isEqualTo("메뉴 3개");
+    }
+
+}
