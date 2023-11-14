5f487102b60a9fbdab1e9280b7245d7c391a84c9
dukcode <duk9741@gmail.com>
2023-11-14 21:53:57 +0900

    test: BadgeResponse 테스트 추가

---
+++ src/test/java/christmas/controller/dto/response/BadgeResponseTest.java
@@ -0,0 +1,37 @@
+package christmas.controller.dto.response;
+
+import christmas.domain.Badge;
+import christmas.domain.Money;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class BadgeResponseTest {
+
+    @Test
+    public void Badge_로_BadgeResponse_를_생성할_수_있다() throws Exception {
+        // given
+        Badge badge = new Badge("배지", Money.of(3000));
+
+        // when
+        // then
+        Assertions.assertThatCode(() -> {
+            BadgeResponse.from(badge);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void toString_메서드_호출_시_Badge_의_이름을_반환한다() throws Exception {
+        // given
+        Badge badge = new Badge("배지", Money.of(3000));
+
+        // when
+        BadgeResponse badgeResponse = BadgeResponse.from(badge);
+
+        // then
+        Assertions.assertThat(badgeResponse.toString()).isEqualTo("배지");
+    }
+
+}
