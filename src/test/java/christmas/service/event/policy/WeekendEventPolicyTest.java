b3066672d16ac37b6d31343aff3a52d4d0eba9e0
dukcode <duk9741@gmail.com>
2023-11-15 03:06:08 +0900

    test: WeekendEventPolicy 테스트 추가

---
+++ src/test/java/christmas/service/event/policy/WeekendEventPolicyTest.java
@@ -0,0 +1,90 @@
+package christmas.service.event.policy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.Menu;
+import christmas.domain.MenuQuantity;
+import christmas.domain.MenuType;
+import christmas.domain.Money;
+import christmas.domain.Order;
+import christmas.domain.Reservation;
+import christmas.domain.ReservationDate;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class WeekendEventPolicyTest {
+
+    @Test
+    public void 예약일이_주말이면_정해진_메뉴타입_당_할인이_적용된다() throws Exception {
+        // given
+        Menu menu1 = new Menu("메뉴1", Money.of(10_000L), MenuType.MAIN_DISH);
+        Menu menu2 = new Menu("메뉴2", Money.of(20_000L), MenuType.APPETIZER);
+        Menu menu3 = new Menu("메뉴3", Money.of(30_000L), MenuType.DESSERT);
+
+        Order order = new Order(List.of(
+                new MenuQuantity(menu1, 1),
+                new MenuQuantity(menu2, 2),
+                new MenuQuantity(menu3, 3)));
+
+        ReservationDate reservationDate1 = new ReservationDate(LocalDate.of(2023, 12, 9)); // 평일
+        ReservationDate reservationDate2 = new ReservationDate(LocalDate.of(2023, 12, 10)); // 주말
+
+        Reservation reservation1 = new Reservation(order, reservationDate1);
+        Reservation reservation2 = new Reservation(order, reservationDate2);
+
+        WeekendEventPolicy weekendEventPolicy1 = new WeekendEventPolicy(MenuType.MAIN_DISH, Money.of(1L));
+        WeekendEventPolicy weekendEventPolicy2 = new WeekendEventPolicy(MenuType.APPETIZER, Money.of(1L));
+        WeekendEventPolicy weekendEventPolicy3 = new WeekendEventPolicy(MenuType.DESSERT, Money.of(1L));
+
+        // when
+        Money discountAmount1 = weekendEventPolicy1.calculateDiscountAmount(reservation1);
+        Money discountAmount2 = weekendEventPolicy1.calculateDiscountAmount(reservation2);
+
+        Money discountAmount3 = weekendEventPolicy2.calculateDiscountAmount(reservation1);
+        Money discountAmount4 = weekendEventPolicy2.calculateDiscountAmount(reservation2);
+
+        Money discountAmount5 = weekendEventPolicy3.calculateDiscountAmount(reservation1);
+        Money discountAmount6 = weekendEventPolicy3.calculateDiscountAmount(reservation2);
+
+        // then
+        assertThat(discountAmount1).isEqualTo(Money.of(1L));
+        assertThat(discountAmount2).isEqualTo(Money.ZERO);
+
+        assertThat(discountAmount3).isEqualTo(Money.of(2L));
+        assertThat(discountAmount4).isEqualTo(Money.ZERO);
+
+        assertThat(discountAmount5).isEqualTo(Money.of(3L));
+        assertThat(discountAmount6).isEqualTo(Money.ZERO);
+    }
+
+    @Test
+    public void 증정품은_존재하지_않는다() throws Exception {
+        // given
+        Menu menu1 = new Menu("메뉴1", Money.of(10_000L), MenuType.MAIN_DISH);
+        Menu menu2 = new Menu("메뉴2", Money.of(20_000L), MenuType.APPETIZER);
+
+        Order order = new Order(List.of(
+                new MenuQuantity(menu1, 1),
+                new MenuQuantity(menu2, 2)));
+
+        ReservationDate reservationDate = new ReservationDate(LocalDate.of(2023, 12, 7)); // 평일
+
+        Reservation reservation = new Reservation(order, reservationDate);
+
+        WeekendEventPolicy weekendEventPolicy = new WeekendEventPolicy(MenuType.MAIN_DISH, Money.of(1L));
+
+        // when
+        Optional<MenuQuantity> gift = weekendEventPolicy.receiveGift(reservation);
+
+        // then
+        assertThat(gift.isEmpty()).isTrue();
+
+    }
+
+}
