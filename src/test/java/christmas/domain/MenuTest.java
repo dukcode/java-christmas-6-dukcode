caf5e78efac590c8104625702ae0028e52696759
dukcode <duk9741@gmail.com>
2023-11-14 23:49:15 +0900

    test: Menu 테스트 추가

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -1,34 +1,65 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class MenuTest {
-//
-//    @Test
-//    public void 메뉴_이름으로_Menu_를_찾을_수_있다() throws Exception {
-//        // given
-//        String name = Menu.BBQ_RIB.getName();
-//
-//        // when
-//        Menu menu = Menu.withName(name);
-//
-//        // then
-//        assertThat(menu).isEqualTo(Menu.BBQ_RIB);
-//    }
-//
-//    @Test
-//    public void 존재하지_않는_메뉴_이름으로_Menu_클래스를_찾을_시_NONE_을_반환한다() throws Exception {
-//        // given
-//        String name = "존재하지 않는 메뉴 이름";
-//
-//        // when
-//        Menu menu = Menu.withName(name);
-//
-//        // then
-//        assertThat(menu).isEqualTo(Menu.NONE);
-//
-//    }
-//
+
+    @Test
+    public void isSameName_호출_시_Menu_이름이_같으면_true_를_반환한다() throws Exception {
+        // given
+        String name = "메뉴";
+        Menu menu = new Menu("메뉴", Money.of(10_000L), MenuType.MAIN_DISH);
+
+        // when
+        boolean isSameName = menu.isSameName(name);
+
+        // then
+        assertThat(isSameName).isEqualTo(true);
+    }
+
+    @Test
+    public void isSameName_호출_시_Menu_이름이_다르면_false_를_반환한다() throws Exception {
+        // given
+        String name = "xxxxx";
+        Menu menu = new Menu("aaaaa", Money.of(10_000L), MenuType.MAIN_DISH);
+
+        // when
+        boolean isSameName = menu.isSameName(name);
+
+        // then
+        assertThat(isSameName).isEqualTo(false);
+    }
+
+    @Test
+    public void isSameMenuType_호출_시_Menu_타입이_같으면_true_를_반환한다() throws Exception {
+        // given
+        MenuType menuType = MenuType.MAIN_DISH;
+        Menu menu = new Menu("aaaaa", Money.of(10_000L), MenuType.MAIN_DISH);
+
+        // when
+        boolean isSameMenuType = menu.isSameMenuType(menuType);
+
+        // then
+        assertThat(isSameMenuType).isEqualTo(true);
+    }
+
+
+    @Test
+    public void isSameMenuType_호출_시_Menu_타입이_다르면_false_를_반환한다() throws Exception {
+        // given
+        MenuType menuType = MenuType.BEVERAGE;
+        Menu menu = new Menu("aaaaa", Money.of(10_000L), MenuType.MAIN_DISH);
+
+        // when
+        boolean isSameMenuType = menu.isSameMenuType(menuType);
+
+        // then
+        assertThat(isSameMenuType).isEqualTo(false);
+    }
+
 }
